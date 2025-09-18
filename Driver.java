43a8295ed1dfe053e677a9c9667a7113c19a0545
Krish Patel <krishpatel8976@gmail.com>
2025-09-18 12:10:01 -0400

    added Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,21 +1,16 @@
-import java.util.Arrays;
-
 public class Driver {
-    public static void main(String [] args) {
-        Polynomial p = new Polynomial();
-        // System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println(Arrays.toString(p1.coefficient));
-        System.out.println(Arrays.toString(p2.coefficient));
-        System.out.println(Arrays.toString(s.coefficient));
-        // System.out.println("s(0.1) = " + s.evaluate(0.1));
-        // if(s.hasRoot(1))
-        //     System.out.println("1 is a root of s");
-        // else
-        //     System.out.println("1 is not a root of s");
+public static void main(String [] args) {
+Polynomial p = new Polynomial();
+System.out.println(p.evaluate(3));
+double [] c1 = {6,0,0,5};
+Polynomial p1 = new Polynomial(c1);
+double [] c2 = {0,-2,0,0,-9};
+Polynomial p2 = new Polynomial(c2);
+Polynomial s = p1.add(p2);
+System.out.println("s(0.1) = " + s.evaluate(0.1));
+if(s.hasRoot(1))
+System.out.println("1 is a root of s");
+else
+System.out.println("1 is not a root of s");
 }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -3,13 +3,11 @@ public class Polynomial{
     public double coefficient[];
 
     public Polynomial(){
-        System.out.println("single construct innit");
         this.coefficient = new double[1];
         coefficient[0] = 0;
     }
 
     public Polynomial(double[] poly){
-        System.out.println("construct innit");
         coefficient = new double[poly.length];
         for (int i = 0; i < poly.length; i++){
             coefficient[i] = poly[i];
@@ -17,7 +15,6 @@ public class Polynomial{
     }
 
     public Polynomial add(Polynomial p2){
-        System.out.println("add innit");
         int len = Math.max(coefficient.length, p2.coefficient.length);
         
         double[] sum = new double[len];
@@ -36,7 +33,27 @@ public class Polynomial{
         }
         return new Polynomial(sum);
 
+        }
+
+
+        public double evaluate(double x){
+            double ans = 0;
+            
+            for (int i=0; i < coefficient.length;i++){
+                ans += (coefficient[i] * Math.pow(x, i));
+            }
+            return ans;
     }
+
+        public boolean hasRoot(double x){
+            if (this.evaluate(x) == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+        }
 }
 
 
